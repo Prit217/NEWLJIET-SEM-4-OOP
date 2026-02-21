82462739f3ec8ca00930abbb13d6b984a06c348a
Prit217 <pritgolakiya21.gamil.com>
2026-02-21 16:57:06 +0530

    changes

--- practical/Q3.java
+++ practical/Q3.java
@@ -3,7 +3,6 @@ class Q3
     
     public static void main(String[] args) 
 	{
-        System.out.print("Enter n: ");
         int n = Integer.parseInt(args[0]);
         
         for (int m = 2, c = 0, i; c < n; m++)
